bff114f33b2933ce012c4cf199cc8cfd88774010
SreeshmaGS <sreeshmags@gmail.com>
2025-01-20 18:02:38 +0530

    Add files via upload
    Day1_HomeAssignments

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float versionType = 91.0f;
+		String developerType = "Google";
+		boolean betaType = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println(versionType + " " + developerType + " " + betaType + " " + releaseYear + " " + shortcutKey);
+		
+		
+	}
+
+}
